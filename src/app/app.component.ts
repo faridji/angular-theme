6d41628de0a1347e18f0e0ff684ed556b85d20f7
faridji <farid.bsse2344@iiu.edu.pk>
2020-01-04 21:03:12 +0500

    fixed background color issue on toggling theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
 
 
@@ -7,7 +7,7 @@ import { OverlayContainer } from '@angular/cdk/overlay';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular Material Theming';
   selectedTheme: string = "pink-theme";
   themes: any[] = ["pink-theme", "candi-theme", "blue-theme"];
@@ -15,6 +15,11 @@ export class AppComponent {
 
   constructor(private overlayContainer: OverlayContainer)
   {
+  }
+
+  ngOnInit() 
+  {
+    document.body.classList.add(this.selectedTheme);  
     this.overlayContainer.getContainerElement().classList.add(this.selectedTheme);
   }
 
@@ -25,6 +30,17 @@ export class AppComponent {
     // Certain components such as menu, select, dialog etc are are inside of a global 
     // overlay container, an additional step is required for those components to be 
     // affected by the theme's css class selector
+    
+    for (let theme of this.themes)
+    {
+      if (this.overlayContainer.getContainerElement().classList.contains(theme))
+      {
+        document.body.classList.remove(theme);
+        this.overlayContainer.getContainerElement().classList.remove(theme);
+      }
+    }
+
+    document.body.classList.add(this.selectedTheme);
     this.overlayContainer.getContainerElement().classList.add(this.selectedTheme);
   }
 }
