836cf93544e259f95e44ce0b8bb48f8825018df8
faridji <farid.bsse2344@iiu.edu.pk>
2020-01-04 13:11:42 +0500

    Toggle 2 themes successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Material Theming';
+  theme: string;
+
+  constructor()
+  {
+    this.theme = "pink-theme";
+  }
+
+  changeTheme(theme: string)
+  {
+    if (theme === "pink")
+    {
+      this.theme = "pink-theme";
+    }
+    else 
+    {
+      this.theme = "blue-theme"
+    }
+  }
 }
